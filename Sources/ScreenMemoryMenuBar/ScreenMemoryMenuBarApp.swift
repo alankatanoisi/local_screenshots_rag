95454b6606ea14f1a15c33925ff81378e660eb22
alankatanoisi <25706761+alankatanoisi@users.noreply.github.com>
2026-03-12 02:50:13 -0700

    Update menu bar icon

--- Sources/ScreenMemoryMenuBar/ScreenMemoryMenuBarApp.swift
+++ Sources/ScreenMemoryMenuBar/ScreenMemoryMenuBarApp.swift
@@ -3,7 +3,7 @@ import SwiftUI
 @main
 struct ScreenMemoryMenuBarApp: App {
     var body: some Scene {
-        MenuBarExtra("ScreenMemory", systemImage: "photo.on.rectangle.angled") {
+        MenuBarExtra("ScreenMemory", systemImage: "photo.badge.magnifyingglass") {
             ContentView()
         }
         .menuBarExtraStyle(.window)
